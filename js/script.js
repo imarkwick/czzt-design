b1cd8854603e53dee040529590e9e202f0d62d7c
clare trembath <claretrembath@ymail.com>
2015-03-17 19:22:49 +0000

    circles fixed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	setTimeout(function() { $("#hi").fadeIn(2000) });
+	setTimeout(function() { $("#hi").fadeOut(100) });
+	setTimeout(function() { $("#im").fadeIn(4000) });
+	setTimeout(function() { $("#im").fadeOut(100) });
+	setTimeout(function() { $("#clare").fadeIn("slow", 6000) });
+
+})
