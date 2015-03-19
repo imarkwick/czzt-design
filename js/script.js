d312855125f12be45f74176fc8c706809c540084
clare trembath <claretrembath@ymail.com>
2015-03-19 19:08:29 +0000

    blur and hello working

--- js/script.js
+++ js/script.js
@@ -1,74 +1,93 @@
-$(document).ready(function(){
-	var whole = (document).getElementById("#whole");
+$(document).ready(function() {
+    setTimeout(function() {
+        $('body').css("-webkit-filter", "blur(4px)")
+    }, 800);
+    setTimeout(function() {
+        $('body').css("-webkit-filter", "blur(3px)")
+    }, 900);
+    setTimeout(function() {
+        $('body').css("-webkit-filter", "blur(2px)")
+    }, 1000);
+    setTimeout(function() {
+        $('body').css("-webkit-filter", "blur(1px)")
+    }, 1100);
+    setTimeout(function() {
+        $('body').css("-webkit-filter", "blur(0px)")
+    }, 1200);
+});
 
-	setTimeout(function() { $('body').css( "-webkit-filter", "blur[i]px")} );
 
-	setTimeout(function() { $("#hi").fadeIn(2000) });
-	setTimeout(function() { $("#hi").fadeOut(100) });
-	setTimeout(function() { $("#im").fadeIn(4000) });
-	setTimeout(function() { $("#im").fadeOut(100) });
-	setTimeout(function() { $("#clare").fadeIn("slow", 8000) });
-	setTimeout(function() { $("#clare").fadeOut(800) });
-})
+var hellos = setTimeout(function() {
+    $("#hi").fadeIn(3000);
+    $("#hi").fadeOut(100);
+    $("#im").fadeIn(5000);
+    $("#im").fadeOut(100);
+    $("#clare").fadeIn(7000);
+    1000
+});
 
+$(window).scroll(function(){
+    $("#clare").css({
+        font: '20px'
+    })
+});
 
-var count = function(){
-
-}
 
 var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
 };
 
 TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-  var that = this;
-  var delta = 300 - Math.random() * 100;
+    var that = this;
+    var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+        delta /= 2;
+    }
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
+    setTimeout(function() {
+        that.tick();
+    }, delta);
 };
 
 window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
     }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
 };
