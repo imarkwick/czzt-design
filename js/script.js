7cd2270038949dc46a9b00a727a012ca59715ff9
clare trembath <claretrembath@ymail.com>
2015-03-20 18:09:51 +0000

    adding scroll down page

--- js/script.js
+++ js/script.js
@@ -26,13 +26,18 @@ var hellos = setTimeout(function() {
     1000
 });
 
-$(window).scroll(function(){
-    $("#clare").css({
-        fontsize: '20px'
-    })
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
 });
-
-
 var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
