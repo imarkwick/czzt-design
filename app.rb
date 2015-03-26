3cb6cadc886d5192a2d0d922ee237ea001e0012d
clare trembath <claretrembath@ymail.com>
2015-03-26 17:02:00 +0000

    all

--- app.rb
+++ app.rb
@@ -1,7 +1,28 @@
 require 'sinatra'
+require 'pony'
 
 class HelloWorldApp < Sinatra::Base
   get '/' do
     erb :index
   end
+
+ post '/' do
+  options = {
+  :to => 'claretrembath@ymail.com',
+  :from => params[:email],
+  :subject => params[:name],
+  :body => params[:message],
+  :via => :smtp,
+  :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'http://www.czzt.co.uk',
+    }
+  }
+  
+  Pony.mail(options)
+  
+  redirect '/'
 end
+end
+
