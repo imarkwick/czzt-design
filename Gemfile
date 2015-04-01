1cd47f0959496a559329241cf8027a4396d1b984
clare trembath <claretrembath@ymail.com>
2015-04-01 15:31:48 +0100

    not reading params

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'pony', '~> 1.11'
-gem 'rack-flash', '~> 0.1.2'
+# gem 'rack-flash', '~> 0.1.2'
+gem 'byebug'
--- app.rb
+++ app.rb
@@ -1,35 +1,64 @@
 require 'sinatra'
 require 'pony'
-require 'rack-flash'
+require 'byebug'
+require 'sinatra/base'
+require 'rack'
+# use Rack::Flash
+
 
 class Application < Sinatra::Base
-  configure do
-  end
   get '/' do
     erb :index
   end
 
  post '/' do
-  send_message
-  flash[:notice] = "Event updated"
+  # params=Rack::Utils.parse_nested_query(request.body.read)
+  @email = params[:emailaddress]
+  @body = params[:body]
+  @name = params[:name]
+
+  puts @email
+  puts @body
+  puts @name
+
+   Pony.mail({
+    :from =>params[:emailaddress],
+    :subject =>params[:name],
+    :body =>params[:body],  
+    :to =>'claretrembath@ymail.com',
+    :via =>:smtp,
+    :via_options => {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true,
+        :domain               => 'localhost.9292',
+  }
+})
+  puts params
+  # flash[:notice] = "Event updated"
   redirect '/'
-end
+  end
 
 
 def send_message
     Pony.mail({
-    :to =>'claretrembath@ymail.com',
     :from =>params[:emailaddress],
     :subject =>params[:from],
     :body =>params[:body],  
+    :to =>'claretrembath@ymail.com',
     :via =>:smtp,
     :via_options => {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true,
+        :domain               => 'localhost.9292',
   }
 })
+end
 end
