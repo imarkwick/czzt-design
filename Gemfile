b3f63ec2501d8d471e45fdefe61e260b9625ae61
clare trembath <claretrembath@ymail.com>
2015-04-01 13:24:41 +0100

    current

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'pony', '~> 1.11'
+gem 'rack-flash', '~> 0.1.2'
--- app.rb
+++ app.rb
@@ -1,32 +1,35 @@
 require 'sinatra'
 require 'pony'
+require 'rack-flash'
 
-class HelloWorldApp < Sinatra::Base
+class Application < Sinatra::Base
+  configure do
+  end
   get '/' do
     erb :index
   end
 
  post '/' do
-  options = {
-  :to => 'claretrembath@ymail.com',
-  :from => params[:email],
-  :subject => params[:name],
-  :body => params[:message],
-  :via => :smtp,
-  :via_options => {
+  send_message
+  flash[:notice] = "Event updated"
+  redirect '/'
+end
+
+
+def send_message
+    Pony.mail({
+    :to =>'claretrembath@ymail.com',
+    :from =>params[:emailaddress],
+    :subject =>params[:from],
+    :body =>params[:body],  
+    :via =>:smtp,
+    :via_options => {
     :address => 'smtp.sendgrid.net',
     :port => '587',
-    :domain => 'http://www.czzt.co.uk',
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
     :enable_starttls_auto => true
-    }
   }
-  
-  Pony.mail(options)
-  
-  redirect '/'
+})
 end
-end
-
