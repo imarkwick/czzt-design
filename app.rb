70d6665aaa5205eb778a5290fb0a477970eef93b
clare trembath <claretrembath@ymail.com>
2015-04-02 14:02:36 +0100

    heroku https change for fonts

--- app.rb
+++ app.rb
@@ -25,29 +25,11 @@ class Application < Sinatra::Base
         :password => ENV['SENDGRID_PASSWORD'],
         :authentication => :plain,
         :enable_starttls_auto => true,
-        :domain               => 'localhost.9292',
+        :domain               => 'czzt.herokuapp.com',
   }
 })
   redirect '/'
   end
 
 
-def send_message
-    Pony.mail({
-    :from =>params[:emailaddress],
-    :subject =>params[:from],
-    :body =>params[:body],  
-    :to =>'claretrembath@ymail.com',
-    :via =>:smtp,
-    :via_options => {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true,
-        :domain               => 'localhost.9292',
-  }
-})
-end
 end
