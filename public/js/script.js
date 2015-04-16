6f0c8ba24c1e0d88d621857ccc0a31b8fe4a0283
clare trembath <claretrembath@ymail.com>
2015-04-16 18:13:55 +0100

    video page for desktop

--- public/js/script.js
+++ public/js/script.js
@@ -16,9 +16,7 @@ $(document).ready(function() {
   }, 1200);
 
 //video
-  setTimeout(function(){
-    $("#englogo").fadeOut(2000);
-}, 2000);
+
 });
 var hellos = setTimeout(function() {
   $("#hi").fadeIn(3000);
